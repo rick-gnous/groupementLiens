234b79f218a7dbbf8d03043d4b8ed309acde1a5c
Rick <rick@gnous.eu>
2020-09-27 01:32:38 +0200

    Ajout des fichiers de base

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request, redirect, url_for
+from os import path
+from bs4 import BeautifulSoup
+
+app = Flask('ui', static_url_path="/static")
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+@app.route('/')
+def slash():
+    return render_template("index.html")
+
+@app.route("/ajout")
+def ajout():
+    return render_template("ajout.html")
+
+@app.route("/apropos")
+def apropos():
+    return render_template("apropos.html")
+
+@app.route("/bizutage", methods=["POST"])
+def bizutage():
+    if request.method == "POST":
+        titre = request.values['titre'] 
+        lien = request.values['lien'] 
+        desc = request.values['desc'] 
+        nouvLien = "<div class=\"elem\"><h2>{}</h2><p><a href=\"{}\">Lien</a></p><hr><p>{}</p>".format(titre, lien, desc)
+        nouvLienHtml = BeautifulSoup(nouvLien, "html.parser")
+        with open("templates/index.html", 'r') as file:
+            soup = BeautifulSoup(file, 'html.parser')
+            soup.find("hr").append(nouvLienHtml)
+        with open("templates/index.html", 'w') as file:
+            file.write(soup.prettify())
+
+        with open("lite/index.html", 'r') as file:
+            soup = BeautifulSoup(file, 'html.parser')
+            soup.find("hr").append(nouvLienHtml)
+        with open("lite/index.html", 'w') as file:
+            file.write(soup.prettify())
+    else:
+        print("error")
+    return redirect(url_for("ajout"))
+
+if __name__ == "__main__":
+    app.run()
