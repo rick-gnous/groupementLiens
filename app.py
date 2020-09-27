0b8aa84ddb2f6a90adb1640d2091ec87ac5787cb
Rick <rick@gnous.eu>
2020-09-27 12:17:06 +0200

    Patch de faille XSS et mise aux normes W3C

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, make_response
 from os import path
 from bs4 import BeautifulSoup
 
@@ -7,7 +7,9 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.route('/')
 def slash():
-    return render_template("index.html")
+    response = make_response(render_template("index.html"))
+    response.headers["Content-Security-Policy"] = "default-src 'self'"
+    return response
 
 @app.route("/ajout")
 def ajout():
@@ -25,15 +27,16 @@ def bizutage():
         desc = request.values['desc'] 
         nouvLien = "<div class=\"elem\"><h2>{}</h2><p><a href=\"{}\">Lien</a></p><hr><p>{}</p>".format(titre, lien, desc)
         nouvLienHtml = BeautifulSoup(nouvLien, "html.parser")
+
         with open("templates/index.html", 'r') as file:
             soup = BeautifulSoup(file, 'html.parser')
-            soup.find("hr").append(nouvLienHtml)
+            soup.find("hr").insert_after("", nouvLienHtml)
         with open("templates/index.html", 'w') as file:
             file.write(soup.prettify())
 
         with open("lite/index.html", 'r') as file:
             soup = BeautifulSoup(file, 'html.parser')
-            soup.find("hr").append(nouvLienHtml)
+            soup.find("hr").insert_after("", nouvLienHtml)
         with open("lite/index.html", 'w') as file:
             file.write(soup.prettify())
     else:
